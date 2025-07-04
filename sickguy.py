00862ea8914f2f6f55964b9e57c5c7dbbbb5b9f9
cybmer2 <oliverpqk@gmail.com>
2025-07-04 08:40:11 +0200

    Adding basic combat & function.

--- sickguy.py
+++ sickguy.py
@@ -9,6 +9,8 @@ player = {
     "speed": 15,
     "gold": 50,
     "potions": 0,
+    "weapon": "Iron Sword",
+    "weapondamage": 6
 
     
 }
@@ -17,24 +19,39 @@ player = {
 
 enemy_templates = [
     {"name": "Goblin", "hp": 10, "dropgold": 5, "attack": 3},
-    {"name:": "Golem", "hp": 40, "dropgold": 20, "attack": 5},
-    {"name:": "Skeleton", "hp": 15, "dropgold": 10, "attack": 5},
+    {"name": "Golem", "hp": 40, "dropgold": 20, "attack": 5},
+    {"name": "Skeleton", "hp": 15, "dropgold": 10, "attack": 5},
 ]
 
 
 def combat(enemy):
-    print(f"\nA wild {enemy["name"]} appears!")
+    print(f"\nA wild {enemy['name']} appears!")
 
     while enemy["hp"] > 0 and player["health"] > 0:
-        print("e")
-        exit()
+        print("\n===FIGHT===")
+        print(f'\n"{player["name"]}" HP: {player["health"]}')
+        print(f"{enemy['name']} HP: {enemy['hp']}")
+        print("1. Attack")
+        print("2. Magic / Healing")
+        print("3. Switch weapon")
+        print("4. Run")
 
+        choice = input("> ")
+
+
+        if choice == "1":
+            print(f"\nYou are wielding an iron sword. Damage: {player['weapondamage']}")
+            print("Attack? Y/N")
+            attackinp = input("> ")
+
+            if attackinp.lower() in ["y", "yes"]:
+                print("e")
 
 def main():
     print("==== WELCOME! ===")
     time.sleep(1)
     print("\nWhat's your name?")
     player["name"] = input("> ")
-    print(f"{player['name']}")
+    combat(enemy_templates[0])
 
 main()
