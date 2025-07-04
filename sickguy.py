95b74238fd16f97a98bb9d9f35d8febb22a6e54c
cybmer2 <oliverpqk@gmail.com>
2025-07-04 10:16:14 +0200

    Adding attacking back & running away, basic attacking functions works now.

--- sickguy.py
+++ sickguy.py
@@ -13,17 +13,21 @@ player = {
     "potions": 0,
     "weapon": "Iron Sword",
     "weapondamage": 6,
-    "weaponluck": 25
+    "weaponluck": 25,
+    "speed": 100,
 
     
 }
 
-
+#lower confidence = higher chance of running away
+#min of 1
+#high confidence = lower chance of running away
+#20 is extremely high confidence, 1 in 20 chance of running away
 
 enemy_templates = [
-    {"name": "Goblin", "hp": 10, "dropgold": 5, "attack": 3},
-    {"name": "Golem", "hp": 40, "dropgold": 20, "attack": 5},
-    {"name": "Skeleton", "hp": 15, "dropgold": 10, "attack": 5},
+    {"name": "Goblin", "hp": 10, "dropgold": 5, "attack": 3, "luck": 10, "confidence": 3,},
+    {"name": "Golem", "hp": 40, "dropgold": 20, "attack": 5, "luck": 0, "confidence": 8,},
+    {"name": "Skeleton", "hp": 15, "dropgold": 10, "attack": 5, "luck": 0, "confidence": 5,},
 ]
 
 
@@ -38,7 +42,7 @@ def clear_screen():
 
 
 def combat(enemy):
-
+    emaxhealth = enemy['hp']
     while enemy["hp"] > 0 and player["health"] > 0:
         clear_screen()
         print("===FIGHT===")
@@ -65,7 +69,8 @@ def combat(enemy):
 
 
                 luck_roll = random.randint(-luck, luck)
-                if luck != 0:
+                if luck != 0: 
+                    clear_screen()
                     if luck_roll == luck:
                         print(f"You got insanely lucky! (+{luck_roll}%)")
                     elif luck_roll >= 0.75 * luck:
@@ -84,12 +89,48 @@ def combat(enemy):
                         print(f"You got mediocrely unlucky. ({luck_roll}%)")
                 
                 if player["damage"] == 0:
-                    clear_screen()
                     damage = ((luck_roll / 100) +1) * player["weapondamage"]
+                    damage = round(damage, 1)
                     enemy["hp"] -= damage
-                    round(enemy["hp"], 1)
-                    print(f"You did {damage} damage!\nThe {enemy['name']} has {enemy['hp']} remaining!")
+                    enemy["hp"] = round(enemy["hp"], 1)
+                    print(f"\nYou did {damage} damage!\nThe {enemy['name']} has {enemy['hp']} health remaining!")
+                    time.sleep(3)
+                else:
+                    damage = ((luck_roll / 100) +1) * player["weapondamage"]
+                    damage = damage + player["damage"]
+                    damage = round(damage, 1)
+                    enemy["hp"] -= damage
+                    enemy["hp"] = round(enemy["hp"], 1)
+                    print(f"\nYour raw strength did {player['damage']} damage!\nIn total you did {damage} damage!\nThe {enemy['name']} has {enemy['hp']} health remaining!")
+                    time.sleep(3) 
                     
+                #attackback
+                clear_screen()
+                luck = enemy['luck']
+                ehealth = enemy['hp']
+                damage = enemy['luck']
+                waittime = random.randint(0.25, 3)
+                print(f"The {enemy['name']} is deciding what to do...")
+                if ehealth < 0.15 * emaxhealth:
+                    #runaway
+
+
+
+                time.sleep(waittime)
+                if luck != 0:
+                    luck_roll = random.randint(-luck, luck)
+                    if luck_roll > 0:
+                        print(f"Your enemmy acquires {luck_roll} extra damage through luck.")
+                    if luck_roll < 0:
+                        print(f"Your enemy looses {luck_roll} damage through luck.")
+                time.sleep(0.5)
+                    
+                
+            
+
+
+
+
 
 
 
